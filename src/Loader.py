4db68aec4f028ab4f1fbaa5f2bb78aefbedff8d7
Devaansh Mann <devaanshmann5@gmail.com>
2026-04-30 12:20:35 -0700

    added functions

---
+++ src/Loader.py
@@ -0,0 +1,12 @@
+import json
+
+def load_documents(path):
+    pass
+
+
+def load_queries(path):
+    pass
+
+def load_qrels(path):
+    pass
+
---
+++ src/Preprocessor.py
@@ -0,0 +1,9 @@
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+
+def preprocess(text):
+   pass
